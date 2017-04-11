4268aca64ea9b3673a22387891926cfa51204f52
Дима <shevchenkodim@rambler.ru>
2017-04-11 14:34:07 +0300

    "First commit"

---
+++ Main.java
@@ -0,0 +1,39 @@
+package com.jdbc;
+
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.*;
+
+/**
+ * Created
+ * by Дима on 28.03.2017.
+ */
+public class Main {
+    private static final String username = "root";
+    private static final String password = "root";
+    private static final String url = "jdbc:mysql://localhost:3306/mysql?useSSL=false";
+
+    public static void main(String[] args) throws SQLException {
+    DBProcessor db = new DBProcessor();
+    Connection conn = db.getConnection(url,username,password);
+    String query = "select * from business.products";
+    Statement statement = conn.createStatement();
+    ResultSet resSet =  statement.executeQuery(query);
+
+    while (resSet.next()){
+        int id;
+        String name;
+        double price;
+        int shopid;
+       id =  resSet.getInt("product_id");
+       name =  resSet.getString("product_name");
+       price =  resSet.getDouble("price");
+       shopid =  resSet.getInt("shop_id");
+       Product product = new Product(id , name , price , shopid);
+       System.out.println(product);
+    }
+
+    statement.close();
+    conn.close();
+    }
+}
---
+++ Product.java
@@ -0,0 +1,22 @@
+package com.jdbc;
+
+/**
+ * Created by Дима on 06.04.2017.
+ */
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int shopID;
+    public Product(){}
+    public Product(int id , String name , double price , int shopID){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.shopID = shopID;
+    }
+    @Override
+    public  String toString(){
+        return "Product:["+ id + "\t" + name +"\t" + price + "грн.\t" + shopID + " ]";
+    }
+}
